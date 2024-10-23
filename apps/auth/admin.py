84b7f6ea498cf758d999411006f4d71f3ff89a15
Rudra Mehta <rudra.mehta@spiculestech.com>
2024-10-23 18:11:53 +0530

    UPDATE: user data table

--- apps/auth/admin.py
+++ apps/auth/admin.py
@@ -4,4 +4,4 @@ from .models import UserModel
 # Register your models here.
 
 
-admin.site.register(UserModel)
+admin.site.register(UserModel, list_display = ['id', 'username', 'email', 'is_superuser', 'is_staff', 'is_active', 'is_deleted'])
