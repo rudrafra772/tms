436c346f2b16fb80262cdf2f7c39ef7eff080ff6
Rudra Mehta <rudra.mehta@spiculestech.com>
2024-10-24 16:20:58 +0530

    CREATE: role and permission

--- apps/user_mgmt/urls.py
+++ apps/user_mgmt/urls.py
@@ -1,7 +1,9 @@
-from .views import UsersView
+from .views import UsersView, RolesAndPermissionView, AddPermissionView
 from django.urls import path
 
 
 urlpatterns = [
     path('users/', UsersView.as_view(), name="users"),
+    path('roles_and_permissions/', RolesAndPermissionView.as_view(), name="r_and_p"),
+    path('add_permission/', AddPermissionView.as_view(), name="add_permission"),
 ]
