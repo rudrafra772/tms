8688f41a58d80f9c9421a39ee4d98b2685051bff
Rudra Mehta <rudra.mehta@spiculestech.com>
2024-12-02 18:25:05 +0530

    CREATE: r and p create and delete

--- apps/user_mgmt/urls.py
+++ apps/user_mgmt/urls.py
@@ -1,4 +1,4 @@
-from .views import UsersView, RolesAndPermissionView, AddPermissionView
+from .views import UsersView, RolesAndPermissionView, AddPermissionView, DeletePermissionView
 from django.urls import path
 
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/', UsersView.as_view(), name="users"),
     path('roles_and_permissions/', RolesAndPermissionView.as_view(), name="r_and_p"),
     path('add_permission/', AddPermissionView.as_view(), name="add_permission"),
+    path('delete_permission/<int:id>/', DeletePermissionView.as_view(), name="delete_permission")
 ]
